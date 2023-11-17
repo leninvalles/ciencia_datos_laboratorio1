b0348fc4893e35e327cc45124442a1c41abef706
leninvalles <lvalllesrios@hotmail.com>
2023-11-17 18:42:17 +0100

    Cambios 3

--- Codigo1.R
+++ Codigo1.R
@@ -1,1 +1,22 @@
-#Practica
+# Pregunta 1: dimensiones data frame
+library(readr)
+epa_http <- read_table("epa-http/epa-http.csv", col_names = FALSE)
+View(epa_http)
+dim(epa_http)
+
+
+
+# Pregunta 2: Valor medio de la columna Bytes
+colnames(epa_http) <- c("IP", "Tiempo", "Tipo", "URL", "Protocolo", "Codigo", "Bytes")
+epa_http$Bytes <- as.numeric(epa_http$Bytes)
+mean(epa_http$Bytes, na.rm = TRUE)
+
+
+
+
+
+
+
+
+
+
