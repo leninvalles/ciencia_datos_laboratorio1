76f154390ff92674f35f39ce1972180bc5faef06
leninvalles <lvallesrios@hotmail.com>
2023-11-17 22:13:15 -0500

    Cambios5

--- Codigo1.R
+++ Codigo1.R
@@ -1,4 +1,10 @@
-# Pregunta 1: dimensiones data frame
+library(readr)
+epa_http <- read_table("D:/Drive(LeninValles)/2. Basicos Capacitaciones/21. Maestria en Ciberseguridad/1. Cursos/12. Ciencia de Datos/Trabajos/ciencia_datos_lab1/epa-http/epa-http.csv", col_names = FALSE)
+View(epa_http)
+
+
+
+# Pregunta 1.1: dimensiones data frame
 library(readr)
 epa_http <- read_table("epa-http/epa-http.csv", col_names = FALSE)
 View(epa_http)
@@ -6,7 +12,7 @@ dim(epa_http)
 
 
 
-# Pregunta 2: Valor medio de la columna Bytes
+# Pregunta 1.2: Valor medio de la columna Bytes
 colnames(epa_http) <- c("IP", "Tiempo", "Tipo", "URL", "Protocolo", "Codigo", "Bytes")
 epa_http$Bytes <- as.numeric(epa_http$Bytes)
 mean(epa_http$Bytes, na.rm = TRUE)
@@ -14,8 +20,15 @@ mean(epa_http$Bytes, na.rm = TRUE)
 #Pregunta 3: Mayor volumen de peticiones HTTP de tipo “GET”
 
 
-
-
+#Pregunta 6: Cantidad de peticiones que NO tienen como protocolo HTTP/0.2
+library(readr)
+epa_http <- read_table("Drive(LeninValles)/2. Basicos Capacitaciones/21. Maestria en Ciberseguridad/1. Cursos/12. Ciencia de Datos/Trabajos/ciencia_datos_lab1/epa-http/epa-http.csv", col_names = FALSE)
+colnames(epa_http) <- c("IP", "Tiempo", "Tipo", "URL", "Protocolo", "Codigo", "Bytes")
+View(epa_http)
+Protocolo <- filter(epa_http, URL != "", Protocolo != "HTTP/0.2")
+mutate(Protocolo, id = row_number())
+summarise (Protocolo, id = row_number())
+View(Protocolo)
 
 
 
